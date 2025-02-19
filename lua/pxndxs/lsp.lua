6c9251dc7c6eeb884de62e86243e0f7a355696b1
Pxndxs 🐼 <pxnditxyr@gmail.com>
2025-02-18 21:35:38 -0400

    refactor: 📏👀✨ separate parentheses for improved code readability and visual clarity

--- lua/pxndxs/lsp.lua
+++ lua/pxndxs/lsp.lua
@@ -3,16 +3,16 @@ return {
   dependencies = {
     {
       'williamboman/mason.nvim',
-      enabled = require('nixCatsUtils').lazyAdd(true, false),
+      enabled = require( 'nixCatsUtils' ).lazyAdd( true, false ),
       config = true,
     },
     {
       'williamboman/mason-lspconfig.nvim',
-      enabled = require('nixCatsUtils').lazyAdd(true, false),
+      enabled = require( 'nixCatsUtils' ).lazyAdd( true, false ),
     },
     {
       'WhoIsSethDaniel/mason-tool-installer.nvim',
-      enabled = require('nixCatsUtils').lazyAdd(true, false),
+      enabled = require( 'nixCatsUtils' ).lazyAdd( true, false ),
     },
 
     { 'j-hui/fidget.nvim', opts = {} },
@@ -20,30 +20,30 @@ return {
     { 'folke/lazydev.nvim', ft = "lua",
       opts = {
         library = {
-          { path = (nixCats.nixCatsPath or "") .. '/lua', words = { "nixCats" } },
+          { path = ( nixCats.nixCatsPath or "" ) .. '/lua', words = { "nixCats" } },
         },
       }
     },
   },
   config = function()
-    vim.api.nvim_create_autocmd('LspAttach', {
-      group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-      callback = function(event)
-        local map = function(keys, func, desc)
-          vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+    vim.api.nvim_create_autocmd( 'LspAttach', {
+      group = vim.api.nvim_create_augroup( 'kickstart-lsp-attach', { clear = true } ),
+      callback = function( event )
+        local map = function( keys, func, desc )
+          vim.keymap.set( 'n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc } )
         end
 
-        map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+        map( '<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame' )
 
-        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+        map( '<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction' )
 
-        map('K', vim.lsp.buf.hover, 'Hover Documentation')
+        map( 'K', vim.lsp.buf.hover, 'Hover Documentation' )
 
-        map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+        map( 'gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration' )
 
-        local client = vim.lsp.get_client_by_id(event.data.client_id)
+        local client = vim.lsp.get_client_by_id( event.data.client_id )
         if client and client.server_capabilities.documentHighlightProvider then
-          local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+          local highlight_augroup = vim.api.nvim_create_augroup( 'kickstart-lsp-highlight', { clear = false } )
           vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
             buffer = event.buf,
             group = highlight_augroup,
@@ -56,22 +56,22 @@ return {
             callback = vim.lsp.buf.clear_references,
           })
 
-          vim.api.nvim_create_autocmd('LspDetach', {
-            group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-            callback = function(event2)
+          vim.api.nvim_create_autocmd( 'LspDetach', {
+            group = vim.api.nvim_create_augroup( 'kickstart-lsp-detach', { clear = true } ),
+            callback = function( event2 )
               vim.lsp.buf.clear_references()
               vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
             end,
-          })
+          } )
         end
 
         if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-          map('<leader>th', function()
-            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-          end, '[T]oggle Inlay [H]ints')
+          map( '<leader>th', function()
+            vim.lsp.inlay_hint.enable( not vim.lsp.inlay_hint.is_enabled() )
+          end, '[T]oggle Inlay [H]ints' )
         end
       end,
-    })
+    } )
 
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     -- capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
@@ -88,7 +88,7 @@ return {
     -- servers.tsserver = {},
     --
 
-    if require('nixCatsUtils').isNixCats then
+    if require( 'nixCatsUtils' ).isNixCats then
       servers.nixd = {}
     else
       servers.rnix = {}
@@ -111,37 +111,35 @@ return {
       },
     }
 
-    if require('nixCatsUtils').isNixCats then
-      for server_name,_ in pairs(servers) do
-        require('lspconfig')[server_name].setup({
+    if require( 'nixCatsUtils' ).isNixCats then
+      for server_name,_ in pairs( servers ) do
+        require( 'lspconfig' )[ server_name ].setup({
           capabilities = capabilities,
-          settings = (servers[server_name] or {}).settings,
-          filetypes = (servers[server_name] or {}).filetypes,
-          cmd = (servers[server_name] or {}).cmd,
-          root_pattern = (servers[server_name] or {}).root_pattern,
+          settings = ( servers[ server_name ] or {} ).settings,
+          filetypes = ( servers[ server_name ] or {} ).filetypes,
+          cmd = ( servers[ server_name ] or {} ).cmd,
+          root_pattern = ( servers[ server_name ] or {} ).root_pattern,
         })
       end
     else
-      require('mason').setup()
+      require( 'mason' ).setup()
 
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
+      local ensure_installed = vim.tbl_keys( servers or {} )
+      vim.list_extend( ensure_installed, {
         'stylua',
-      })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+      } )
+      require( 'mason-tool-installer' ).setup { ensure_installed = ensure_installed }
 
-      require('mason-lspconfig').setup {
+      require( 'mason-lspconfig' ).setup {
         handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
+          function( server_name )
+            local server = servers[ server_name ] or {}
+            server.capabilities = vim.tbl_deep_extend( 'force', {}, capabilities, server.capabilities or {} )
+            require( 'lspconfig' )[ server_name ].setup( server )
           end,
         },
       }
     end
   end,
 }
-
-
 
