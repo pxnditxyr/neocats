c7bab6b9ba7bc1e921b226afcc0d704b55cce67a
Pxndxs 🐼 <pxnditxyr@gmail.com>
2025-03-14 20:47:17 -0400

    refactor: 🖥️🔧✨ update cmdline disabling logic: deactivate in WSL and activate in Linux for better compatibility

--- lua/pxndxs/completion.lua
+++ lua/pxndxs/completion.lua
@@ -1,3 +1,11 @@
+local is_wsl = (function()
+  local os_uname = vim.loop.os_uname()
+  local version = os_uname.version:lower()
+  local release = os_uname.release:lower()
+  return version:find('microsoft') ~= nil or version:find('wsl') ~= nil
+      or release:find('microsoft') ~= nil or release:find('wsl') ~= nil
+end)()
+
 return {
   {
     'saghen/blink.compat',
@@ -76,8 +84,9 @@ return {
       },
 
       cmdline = {
-        enabled = false
+        enabled = not is_wsl
       },
+
       sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer', 'emoji' },
         per_filetype = {
