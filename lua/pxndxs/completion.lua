7f34ca235855367fc5e5fb6b63016c1a5a5f4a92
Pxndxs 🐼 <pxnditxyr@gmail.com>
2025-03-19 20:22:34 -0400

    feat: 🖥️📚✨ integrate completion.documentation in Blink CMP to display detailed function information during autocompletion

--- lua/pxndxs/completion.lua
+++ lua/pxndxs/completion.lua
@@ -105,6 +105,12 @@ return {
           },
         }
       },
+      completion = {
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 500,
+        },
+      },
       -- completion = {
       --   menu = {
       --     auto_show = function( ctx )
