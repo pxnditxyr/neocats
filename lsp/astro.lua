e59513c14b287b3046f38e242614a1ed2b504799
Pxndxs 🐼 <pxnditxyr@gmail.com>
2025-07-13 18:17:02 -0400

    chore: 🛠️📡⚙️ Refine Astro LSP configuration to leverage TypeScript SDK integration chore: 🐧🔄🆙 Update neocats Nix flake to latest revision

--- lsp/astro.lua
+++ lsp/astro.lua
@@ -1,14 +1,25 @@
+local function get_tsserver_path(root_dir)
+  local node_modules = vim.fs.find('node_modules', { path = root_dir, upward = true })[ 1 ]
+  if node_modules then
+    local project_root = vim.fs.dirname( node_modules )
+    local ts_path = vim.fs.joinpath(
+      project_root, 'node_modules', 'typescript', 'lib', 'tsserverlibrary.js'
+    )
+    if vim.loop.fs_stat( ts_path ) then
+      return ts_path
+    end
+  end
+  return ''
+end
+
 return {
   cmd = { "astro-ls", "--stdio" },
   filetypes = { "astro" },
-  root_markers = { "astro.config.mjs" },
+  root_markers = { 'package.json', 'tsconfig.json', 'jsconfig.json', '.git' },
   single_file_support = true,
   init_options = {
     typescript = {
-      tsdk = "node_modules/typescript/lib",
+      tsdk = get_tsserver_path( vim.fn.getcwd() ),
     }
   },
-  before_init = function( params )
-    print( params )
-  end
 }
