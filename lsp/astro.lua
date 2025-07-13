487ca3f2573f1ddcef6fc70cbc859e98e2118985
Pxndxs 🐼 <pxnditxyr@gmail.com>
2025-07-13 18:28:34 -0400

    fix: 🔙⚙️🐛 Revert Astro configuration to previous version to avoid “TSDK not found” error

--- lsp/astro.lua
+++ lsp/astro.lua
@@ -1,17 +1,3 @@
-local function get_tsserver_path(root_dir)
-  local node_modules = vim.fs.find('node_modules', { path = root_dir, upward = true })[ 1 ]
-  if node_modules then
-    local project_root = vim.fs.dirname( node_modules )
-    local ts_path = vim.fs.joinpath(
-      project_root, 'node_modules', 'typescript', 'lib', 'tsserverlibrary.js'
-    )
-    if vim.loop.fs_stat( ts_path ) then
-      return ts_path
-    end
-  end
-  return ''
-end
-
 return {
   cmd = { "astro-ls", "--stdio" },
   filetypes = { "astro" },
@@ -19,7 +5,7 @@ return {
   single_file_support = true,
   init_options = {
     typescript = {
-      tsdk = get_tsserver_path( vim.fn.getcwd() ),
+      tsdk = "node_modules/typescript/lib"
     }
   },
 }
