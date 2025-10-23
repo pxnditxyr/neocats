e802f207b2df7dcd568d02c3b74d8b4cfbdceb40
Pxndxs 🐼 <pxnditxyr@gmail.com>
2025-10-23 09:27:35 -0400

    feat: ✨🛠️💻 Add **color highlight display** for Tailwind and other colors to the **completion configuration**

--- lua/pxndxs/completion.lua
+++ lua/pxndxs/completion.lua
@@ -110,7 +110,34 @@ return {
           auto_show = true,
           auto_show_delay_ms = 500,
         },
-      },
+        menu = {
+          draw = {
+            components = {
+              kind_icon = {
+                text = function(ctx)
+                  local icon = ctx.kind_icon
+                  if ctx.item.source_name == "LSP" then
+                    local color_item = require("nvim-highlight-colors").format(ctx.item.documentation, { kind = ctx.kind })
+                    if color_item and color_item.abbr ~= "" then
+                      icon = color_item.abbr
+                    end
+                  end
+                  return icon .. ctx.icon_gap
+                end,
+                highlight = function(ctx)
+                  local highlight = "BlinkCmpKind" .. ctx.kind
+                  if ctx.item.source_name == "LSP" then
+                    local color_item = require("nvim-highlight-colors").format(ctx.item.documentation, { kind = ctx.kind })
+                    if color_item and color_item.abbr_hl_group then
+                      highlight = color_item.abbr_hl_group
+                    end
+                  end
+                  return highlight
+                end,
+              }
+            }
+          }
+        },
       -- completion = {
       --   menu = {
       --     auto_show = function( ctx )
@@ -122,6 +149,7 @@ return {
       --     end,
       --   },
       -- },
+      }
     },
     opts_extend = { "sources.default" },
     config = function( _, opts )
