3b6912a8cbdbc43aa6a881747a8d31bb91fc9755
Pxndxs 🐼 <pxnditxyr@gmail.com>
2025-12-15 11:18:29 -0400

    refactor: 🛠️💻✨ Change the **keymap** for the **debug toggle** from `db` to `dT` to prevent collision with the **`dadbod-ui`** `db` keymap

--- lua/custom/plugins/dap-ui.lua
+++ lua/custom/plugins/dap-ui.lua
@@ -10,7 +10,7 @@ return {
 
   keys = {
     { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
-    { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+    { "<leader>dT", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
     { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
     { "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
     { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
